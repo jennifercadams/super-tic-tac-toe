be8f344e443f1dc1a0916ea125495679cced4df4
Jennifer Adams <jennadamscodes@gmail.com>
2025-03-16 19:42:41 -0400

    Create SingleGame component

---
+++ src/components/SingleGame/SingleGame.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import { useState } from "react";
+import Board from "~components/Board/Board";
+import { checkForWinner } from "~helpers/gameHelper";
+import { Player } from "~types";
+
+const SingleGame = () => {
+    const [ player, setPlayer ] = useState<string>(Player.X);
+    const [ squares, setSquares ] = useState<string[]>(Array(9).fill(""));
+    const [ winner, setWinner ] = useState<string | null>(null);
+
+    const handleClick = (i: number) => {
+        if (squares[i])
+            return;
+
+        const nextSquares = squares.slice();
+        nextSquares[i] = player;
+        setSquares(nextSquares);
+
+        const nextPlayer = player == Player.X ? Player.O : Player.X;
+        setPlayer(nextPlayer);
+
+        setWinner(checkForWinner(nextSquares));
+    };
+
+    const boardProps = { winner, squares, handleClick };
+
+    return (
+        <Board {...boardProps} />
+    );
+};
+
+export default SingleGame;
