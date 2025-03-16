cce0c447bfb18c327d1a367200ce52a4f47fff38
Jennifer Adams <jennadamscodes@gmail.com>
2025-03-16 17:21:31 -0400

    Create SuperBoard component and lift state from Board

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import Board from "~components/Board/Board";
+import SuperBoard from "~components/SuperBoard/SuperBoard";
 import "./App.css";
 
 const App = () => (
   <>
     <h1>Super Tic Tac Toe</h1>
-    <Board />
+    <SuperBoard />
   </>
 );
 
--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,32 +1,15 @@
 import * as React from "react";
-import { useState } from "react";
 import Square from "~components/Square/Square";
-import { checkForWinner } from "~helpers/gameHelper";
 import "./Board.css";
 
-export enum Player {
-    X = "X",
-    O = "O"
-}
+export type BoardProps = {
+    winner: string | null;
+    squares: string[];
+    handleClick: (arg: number) => void;
+};
 
-const Board = () => {
-    const [ player, setPlayer ] = useState<string>(Player.X);
-    const [ squares, setSquares ] = useState<string[]>(Array(9).fill(""));
-    const [ winner, setWinner ] = useState<string | null>(null);
-
-    const handleClick = (i: number) => {
-        if (squares[i])
-            return;
-
-        const nextSquares = squares.slice();
-        nextSquares[i] = player;
-        setSquares(nextSquares);
-
-        const nextPlayer = player == Player.X ? Player.O : Player.X;
-        setPlayer(nextPlayer);
-
-        setWinner(checkForWinner(nextSquares));
-    };
+const Board = (props: BoardProps) => {
+    const { winner, squares, handleClick } = props;
 
     return (
         <div className="board">
