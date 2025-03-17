e6ea24144aa84dee261fbe8559878adc8afdec4c
Jennifer Adams <jennadamscodes@gmail.com>
2025-03-17 02:52:38 -0400

    Install gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
+    base: '/super-tic-tac-toe/',
     plugins:[
         tsconfigPaths(),
     ],
